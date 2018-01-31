3f096fac542a7b7ca245351e6f163b5b4ab02be2
Stephan Mc Donald <stephan.mcdonald@rewe-digital.com>
2018-01-31 10:42:41 +0100

    moved js to external file

---
+++ site/static/js/slider.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.slick-track').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        nextArrow: '<button type="button" class="slick-direction slick-next slick-arrow" id="nextArrow-69589edc-bbd5-4d7a-8065-d63b400d8ca9" style="">\n' +
+        '            <svg>\n' +
+        '                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#inline-icon-carrot-right"></use>\n' +
+        '            </svg>\n' +
+        '        </button>',
+        prevArrow: '<button type="button" class="slick-direction slick-prev slick-arrow" id="prevArrow-69589edc-bbd5-4d7a-8065-d63b400d8ca9" style="">\n' +
+        '            <svg>\n' +
+        '                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#inline-icon-carrot-left"></use>\n' +
+        '            </svg>\n' +
+        '        </button>'
+    });
+});
