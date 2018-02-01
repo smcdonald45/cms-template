966e85319c28c8678807eeabca542681877bc92b
Stephan Mc Donald <stephan.mcdonald@rewe-digital.com>
2018-02-01 10:53:25 +0100

    moved js

---
+++ site/static/js/recipe.js
@@ -0,0 +1,14 @@
+$('#recipe-head-image').hover(
+    function(e) {
+        $('#edit-recipe-btn').show();
+    },function() {
+        $('#edit-recipe-btn').hide();
+    }
+);
+$('#edit-recipe-btn').hover(
+    function(e) {
+        $('#edit-recipe-btn').show();
+    },function() {
+        $('#edit-recipe-btn').hide();
+    }
+);
